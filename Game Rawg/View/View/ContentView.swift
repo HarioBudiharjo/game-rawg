8777500077ada177ea00ec72e1f0b3efd211cf6e
Hario Budiharjo <hariobudiharjo@Gmail.com>
2020-07-17 00:23:59 +0700

    tabbar home

--- Game Rawg/View/View/ContentView.swift
+++ Game Rawg/View/View/ContentView.swift
@@ -10,7 +10,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        TabView {
+            HomeView()
+                .tabItem {
+                    Image(systemName: "house")
+                    Text("Home")
+            }
+            AboutView()
+                .tabItem {
+                    Image(systemName: "person")
+                    Text("About")
+            }
+        }
     }
 }
 
