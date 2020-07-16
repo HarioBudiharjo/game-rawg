c59eb83e5be11d77a860ee851b8c07809542f819
Hario Budiharjo <hariobudiharjo@Gmail.com>
2020-07-16 23:07:07 +0700

    Indicator actibity loading

---
+++ Game Rawg/View/Custom/ActivityIndicator.swift
@@ -0,0 +1,40 @@
+//
+//  ActivityIndicator.swift
+//  Game Rawg
+//
+//  Created by Hario Budiharjo on 08/07/20.
+//  Copyright © 2020 Hario Budiharjo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ActivityIndicator: View {
+    
+    @State private var degress = 0.0
+    let size: CGFloat
+    var body: some View {
+        Circle()
+            .trim(from: 0.0, to: 0.8)
+            .stroke(lineWidth: 5.0)
+            .frame(width: size, height: size)
+            .rotationEffect(Angle(degrees: degress))
+            .onAppear{ self.start()}
+    }
+    
+    func start() {
+        Timer.scheduledTimer(withTimeInterval: 0.02, repeats: true) { timer in
+            withAnimation {
+                self.degress += 10.0
+            }
+            if self.degress == 360.0 {
+                self.degress = 0.0
+            }
+        }
+    }
+}
+
+struct ActivityIndicator_Previews: PreviewProvider {
+    static var previews: some View {
+        ActivityIndicator(size: 75)
+    }
+}
