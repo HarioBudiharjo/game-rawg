84e7de0f89d17324ab020e0131caf3cfa73af54f
Hario Budiharjo <hariobudiharjo@Gmail.com>
2020-07-22 20:39:19 +0700

    Change double extension to split file

---
+++ Game Rawg/Extension/ExtensionDouble.swift
@@ -0,0 +1,15 @@
+//
+//  ExtensionDouble.swift
+//  Game Rawg
+//
+//  Created by Hario Budiharjo on 17/07/20.
+//  Copyright © 2020 Hario Budiharjo. All rights reserved.
+//
+
+import Foundation
+
+extension Double {
+    func format() -> String {
+        return String(format: "%.2f",self)
+    }
+}
--- Game Rawg/View/Row/GameRow.swift
+++ Game Rawg/View/Row/GameRow.swift
@@ -69,15 +69,3 @@ struct GameRow: View {
         }.frame(height: 130)
     }
 }
-
-extension Double {
-    func format() -> String {
-        return String(format: "%.2f",self)
-    }
-}
-
-struct GameRow_Previews: PreviewProvider {
-    static var previews: some View {
-        GameRow(game: Game(id: 1, judul: "hello", gambar: "https://avatars2.githubusercontent.com/u/10007846?s=460&u=dd0a404a51a0d26d9035843559a22bc2adbf0bc6&v=4", tanggalRilis: "18 Juli 1997", peringkat: 4.9))
-    }
-}
