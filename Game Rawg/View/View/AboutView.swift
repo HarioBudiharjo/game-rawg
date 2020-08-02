5abd76cffe4f3202b35b871f67e6ea5350a303be
Hario Budiharjo <hariobudiharjo@Gmail.com>
2020-08-03 00:02:08 +0700

    allow edit profile

--- Game Rawg/View/View/AboutView.swift
+++ Game Rawg/View/View/AboutView.swift
@@ -9,6 +9,11 @@
 import SwiftUI
 
 struct AboutView: View {
+    @State var edit : Bool = false
+    @State var nama = ""
+    @State var email = ""
+    @State var githubUrl = ""
+    @State var githubName = ""
     var body: some View {
         NavigationView {
             VStack{
@@ -20,27 +25,46 @@ struct AboutView: View {
                 
                 Divider()
                 
-                Text("Hario Budiharjo")
-                Text("hariobudiharjo@gmail.com")
-                
-                Button(action: {
-                    let urlGithub = "https://github.com/HarioBudiharjo"
-                    let url: NSURL = URL(string: urlGithub)! as NSURL
-                    
-                    UIApplication.shared.open(url as URL)
-                    
-                }) {
-                    Text("Github : HarioBudiharjo")
+                if edit {
+                    TextField("Nama", text: $nama).textFieldStyle(RoundedBorderTextFieldStyle())
+                    TextField("Email", text: $email).textFieldStyle(RoundedBorderTextFieldStyle())
+                    TextField("Github Url", text: $githubUrl).textFieldStyle(RoundedBorderTextFieldStyle())
+                    TextField("Github Name", text: $githubName).textFieldStyle(RoundedBorderTextFieldStyle())
+                } else {
+                    Text(nama)
+                    Text(email)
+                    Button(action: {
+                        let urlGithub = self.githubUrl
+                        let url: NSURL = URL(string: urlGithub)! as NSURL
+                        
+                        UIApplication.shared.open(url as URL)
+                        
+                    }) {
+                        Text("Github : \(githubName)")
+                    }
                 }
-                
             }
             .navigationBarTitle(Text("Profile"))
+            .navigationBarItems(trailing:
+                Button(edit ? "Save" : "Edit"){
+                    if self.edit {
+                        self.saveData()
+                    }
+                    self.edit = !self.edit
+                }
+            )
+        }.onAppear{
+            self.nama = SharedPref.getName() ?? "Name empty!"
+            self.email = SharedPref.getEmail() ?? "Email empty!"
+            self.githubUrl = SharedPref.getGithubUrl() ?? "Github url empty!"
+            self.githubName = SharedPref.getGithubName() ?? "Github Name empty!"
         }
     }
-}
-
-struct AboutView_Previews: PreviewProvider {
-    static var previews: some View {
-        AboutView()
+    
+    func saveData(){
+        SharedPref.saveName(name: nama)
+        SharedPref.saveEmail(email: email)
+        SharedPref.saveGithubUrl(githubUrl: githubUrl)
+        SharedPref.saveGithubName(githubName: githubName)
     }
 }
